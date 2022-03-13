7f6839f2f7f9f22039b91d9cad46e4e56b173220
conallmimnagh <conallmim@gmail.com>
2022-03-13 13:14:32 +0000

    Removing unsed using statements for poling service for mims muscle website

--- MonitoringService/Worker.cs
+++ MonitoringService/Worker.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 
 namespace MonitoringService
